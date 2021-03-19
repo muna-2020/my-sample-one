f12445c3f0fab400020082f7a945865c2da151cc
Muna Sethi <muna.sethi@arcadix.com>
2021-03-19 19:47:02 +0530

    Updates

---
+++ mono-config.js
@@ -0,0 +1,6 @@
+config = {
+ 	vfs_prefix: "managed",
+ 	deploy_prefix: "managed",
+ 	enable_debugging: 0,
+ 	file_list: [ "Sample.dll","Aspose.Cells.dll","System.Core.dll","mscorlib.dll","System.dll","Mono.Security.dll","System.Xml.dll","System.Numerics.dll","WebAssembly.Net.WebSockets.dll","netstandard.dll","System.Data.dll","System.Transactions.dll","System.Data.DataSetExtensions.dll","System.Drawing.Common.dll","System.IO.Compression.dll","System.IO.Compression.FileSystem.dll","System.ComponentModel.Composition.dll","System.Net.Http.dll","System.Net.Http.WebAssemblyHttpHandler.dll","WebAssembly.Bindings.dll","System.Memory.dll","System.Runtime.Serialization.dll","System.ServiceModel.Internals.dll","System.Xml.Linq.dll","System.Web.dll","System.Configuration.dll","System.Security.dll","System.Drawing.dll","System.Runtime.Serialization.Formatters.Soap.dll","System.EnterpriseServices.dll","System.Web.ApplicationServices.dll","System.ComponentModel.DataAnnotations.dll","Mono.Data.Sqlite.dll","System.Web.Services.dll","System.DirectoryServices.dll","Novell.Directory.Ldap.dll","System.Design.dll","System.Windows.Forms.dll","Accessibility.dll","Mono.WebBrowser.dll","Mono.Posix.dll" ],
+}
